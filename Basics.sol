a5e479e62e71ab7906b4576ea44f41833d0ca44b
pranita-2145 <bpranita2005@gmail.com>
2025-12-20 00:31:05 +0530

    Create Basics.sol

---
+++ Basics.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract{
+    // DataTypes
+    string public mystring = "Hello World";
+    bool public boolean1 = true;
+    uint public myUnit = 1;
+    int public myInt = 1;
+    //address public myAddress = 1FfmbHfnpaZjKFvyi1okTjJJusN455paPH;
+
+}
